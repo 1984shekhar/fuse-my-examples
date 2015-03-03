2ce7b63b381fa5e732e0da128486fdc7bbfa5a8d
cpandey <shekhar.csp84@yahoo.co.in>
2015-03-03 17:58:47 +0530

    Jasypt-http4-PropertiesComponent Example

---
+++ Jasypt-http4-PropertiesCom-fabric/src/main/java/com/mycompany/route/Venue.java
@@ -0,0 +1,27 @@
+package com.mycompany.route;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class Venue extends RouteBuilder {
+
+    public void configure() {
+
+        venues();
+
+    }
+
+    // --------------------------------------------------------------
+    // Configures Zone Entry event sync flow
+    private void venues(){
+
+        //@formatter:off
+        from("timer://foo?fixedRate=true&period=10000")
+        .id("DEMO-venue")
+            .to("http4://localhost:8080/Test/printIncomingParameters.jsp?name={{rest.username}}&passwd={{rest.password}}")
+            .to("log:DEMO-CAMEL?level=TRACE&showAll=true");
+
+    }
+
+
+}
+
