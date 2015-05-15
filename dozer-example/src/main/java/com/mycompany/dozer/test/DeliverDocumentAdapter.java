9ef471798c60a342e73a47a784407532a104af49
cpandey <shekhar.csp84@yahoo.co.in>
2015-05-15 12:51:29 +0530

    Dozer is a Java Bean to Java Bean mapper that recursively copies data from one object to another. Typically, these Java Beans will be of different complex types.

---
+++ dozer-example/src/main/java/com/mycompany/dozer/test/DeliverDocumentAdapter.java
@@ -0,0 +1,24 @@
+package com.mycompany.dozer.test;
+
+import org.apache.camel.Body;
+
+
+public class DeliverDocumentAdapter extends ModelAdapter {
+    
+	/*
+	 * map Request to Response
+	 * 
+	 */
+	
+	public String mapDeliverDocumentResponse(@Body String testInput){
+		 
+		Request request = new Request();
+		request.setA(testInput);
+		request.setB(testInput);
+				
+		Response response = getMapper().map(request, Response.class, "req_resp");
+		return response.getA1() + ":" + response.getB1();
+			
+	}
+	
+}
