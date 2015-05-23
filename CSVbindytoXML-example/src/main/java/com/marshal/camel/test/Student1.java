f48abaf50d25faad940933428c5bb3d29f91b75c
cpandey <shekhar.csp84@yahoo.co.in>
2015-05-23 23:55:29 +0530

    csv to xml using camel-bindy and jaxb marshalling

---
+++ CSVbindytoXML-example/src/main/java/com/marshal/camel/test/Student1.java
@@ -0,0 +1,82 @@
+
+
+package com.marshal.camel.test;
+
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.naming.Context;
+import javax.naming.Name;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.bindy.model.ObjectFactory;
+import org.apache.camel.bindy.model.Order;
+import org.apache.camel.bindy.model.OrderBase;
+import org.apache.log4j.Logger;
+
+
+public class Student1 implements Processor{
+	
+	
+	@Override
+	public void process(Exchange arg0) throws Exception {
+		// TODO Auto-generated method stub
+		 Logger log = Logger.getLogger(getClass());
+		log.info("Within Student1 Processor...");
+		List<Map<String, Order>> unmarshaledModels = (List<Map<String, Order>>) arg0.getIn().getBody();
+		
+		ObjectFactory objectFactory = new ObjectFactory();
+		OrderBase orderBase = objectFactory.createOrderBase();
+
+		Iterator<Map<String, Order>> orderIterator = unmarshaledModels.iterator();
+      while (orderIterator.hasNext()) {
+         Map<String, Order> orderMap = orderIterator.next();
+         
+         orderBase.setOrder(orderMap.get(Order.class.getName()));
+      }
+      arg0.getIn().setBody(orderBase);
+	
+
+		/*int modelCount = 0;
+		
+		
+		List<Map<String, Object>> list; // this is what you have already
+
+		for (Map<String, Object> map : unmarshaledModels) {
+			log.info("Within Map: "+map.size());
+		    for (Map.Entry<String, Object> entry : map.entrySet()) {
+		        String key = entry.getKey();
+		        Object value = entry.getValue();
+		        
+		        Order order=(Order) value;
+		        
+		        
+		        log.info("key "+key +"    value" +value.toString());
+		        
+		        log.info("    DATA IN VALUE (ORDER OBJECT) "+order.getLabelState() );
+		        
+		    }
+		}
+		
+		*/
+		/*for (Map<String, Object> model : unmarshaledModels) {
+		  for (String className : model.keySet()) {
+		     Object obj = model.get(className);
+		     log.info("CLASS NAME "+className);
+		     
+		     log.info("Count : " + modelCount + ", " + obj.toString());
+		  }
+		 modelCount++;
+		}
+		*/
+	}
+	
+	
+	
+
+	
+	 
+}
---
+++ CSVbindytoXML-example/src/main/java/org/apache/camel/bindy/model/OrderBase.java
@@ -0,0 +1,73 @@
+package org.apache.camel.bindy.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlType;
+
+import org.apache.log4j.Logger;
+
+/*@XmlSeeAlso(Order.class)*/
+
+@XmlType(name = "OrderBase", propOrder = { "orders" })
+@XmlRootElement(name = "OrderBase")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class OrderBase implements Serializable {
+
+	@XmlElement(name = "order")
+	protected List<Order> orders = null;
+
+	public List<Order> getOrders() {
+		if (orders == null) {
+			orders = new ArrayList<Order>();
+		}
+		return this.orders;
+	}
+
+	public OrderBase() {
+
+		Logger log = Logger.getLogger(getClass());
+		log.info("REACHED MARSHALLER");
+
+	}
+
+	public void setOrder(Order order) {
+		this.getOrders().add(order);
+	}
+
+	/*
+	 * @XmlRootElement
+	 * 
+	 * @XmlAccessorType(XmlAccessType.FIELD)
+	 * 
+	 * @XmlType(name = "", propOrder = { "idState", "LabelState",
+	 * 
+	 * }) public static class Order implements Serializable{ private static
+	 * final long serialVersionUID = 1L;
+	 * 
+	 * @XmlElement(required = true) protected String idState;
+	 * 
+	 * 
+	 * @XmlElement(required = true) protected String LabelState;
+	 * 
+	 * 
+	 * public String getIdState() { return idState; }
+	 * 
+	 * 
+	 * public void setIdState(String idState) { this.idState = idState; }
+	 * 
+	 * 
+	 * public String getLabelState() { return LabelState; }
+	 * 
+	 * 
+	 * public void setLabelState(String labelState) { LabelState = labelState; }
+	 * 
+	 * }
+	 */
+}
